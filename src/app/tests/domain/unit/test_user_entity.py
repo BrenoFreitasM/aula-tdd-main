a6c21ddf0ae4e44dbb1a7b873f20bfc7d6fb784a
Breno Freitas <brenofreitasmendes@gmail.com>
2025-01-24 04:23:04 -0300

    first commit

---
+++ src/app/tests/domain/unit/test_user_entity.py
@@ -0,0 +1,12 @@
+from uuid import uuid4
+
+class TestUser:
+
+    # TESTE PARA CONSTRUIR O USUÁRIO
+    def test_user_initialization(self):
+        user_id = uuid4()
+        user_name = "Alexandre"
+        user = User(id=user_id, name=user_name)
+
+        assert user.id == user_id
+        assert user.name == user_name
